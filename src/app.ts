ddc18ef2425e76b76fa1c8c7abb2fbfed0b50164
aklidata <aklidata.co@gmail.com>
2022-06-19 11:55:47 -0400

    added database managment

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import { dbInitialize } from './db-connect';
+import { addCustomer } from './db-manager';
 
-console.log("Hi, dicks");
+console.log("Hello pale blue dot.");
 
 dbInitialize();
+
+addCustomer({ name: "Yahya", email: "yahya@yahya.com", phone_number: 57155 });
