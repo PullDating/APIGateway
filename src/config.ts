54758c22c348c62af95aa6d2a621493b4bae914a
WilliamDormer <60399584+WilliamDormer@users.noreply.github.com>
2022-07-11 11:44:48 -0400

    Delete config.ts

--- src/config.ts
+++
@@ -1,9 +0,0 @@
-import env from 'env-var';
-
-export const SERVICE_PORT = 3000;
-
-export const DB_URL = "rdb.dev.pull.dating";
-export const DB_PORT = "5432";
-export const DB_NAME = "pulldevwill";
-export const DB_USER = "postgres";
-export const DB_PASS = "sWkLEYkPuVUQbj4cnLFrVPDBjqHbdN";
