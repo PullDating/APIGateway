007c03c03dc1749bcddeae6fae570efa0e7cd85e
Gustavo Lima <11997211+guhenrique007@users.noreply.github.com>
2022-07-23 14:33:43 -0300

    Initial docker

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # APIGateway
 The node js server that will act as the gateway for microservices (atm acting as the entire api server)
+
+# Run on docker 
+docker-compose up --build -d
---
+++ src/config/vars.ts
@@ -0,0 +1,6 @@
+// may have a env validator here
+export const SERVICE_PORT = process.env.SERVICE_PORT || 3000;
+export const DB_URL = process.env.DB_URL || 'postgres';
+export const DB_NAME = process.env.DB_NAME || 'postgres';
+export const DB_USER = process.env.DB_USER || 'postgres';
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'postgres';
